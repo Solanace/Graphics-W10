46eda63f6861f91a09069c6409c2eb1f745bf82e
Penn Wu <penn.z.wu@gmail.com>
2018-05-02 11:44:29 -0400

    Finished

--- gmath.c
+++ gmath.c
@@ -9,35 +9,61 @@
 //lighting functions
 color get_lighting(double *normal, double *view, color alight, double light[2][3], double *areflect, double *dreflect, double *sreflect) {
 	color i;
-	i.red = 0;
-	i.green = 0;
-	i.blue = 0;
+
+	normalize(normal);
+	normalize(view);
+	normalize(light[LOCATION]);
+
+	color a = calculate_ambient(alight, areflect);
+	color d = calculate_diffuse(light, dreflect, normal);
+	color s = calculate_specular(light, sreflect, view, normal);
+
+	i.red = a.red + d.red + s.red;
+	i.green = a.green + d.green + s.green;
+	i.blue = a.blue + d.blue + s.blue;
 	limit_color(&i);
 	return i;
 }
 
 color calculate_ambient(color alight, double *areflect) {
 	color a;
+	a.red = alight.red * areflect[RED];
+	a.green = alight.green * areflect[GREEN];
+	a.blue = alight.blue * areflect[BLUE];
+	//printf("Color: %d %d %d\n:", a.red, a.green, a.blue);
 	return a;
 }
 
 color calculate_diffuse(double light[2][3], double *dreflect, double *normal) {
 	color d;
+	d.red = light[COLOR][RED] * dreflect[RED] * dot_product(normal, light[LOCATION]);
+	d.green = light[COLOR][GREEN] * dreflect[GREEN] * dot_product(normal, light[LOCATION]);
+	d.blue = light[COLOR][BLUE] * dreflect[BLUE] * dot_product(normal, light[LOCATION]);
+	//printf("Color: %d %d %d\n:", d.red, d.green, d.blue);
 	return d;
 }
 
 color calculate_specular(double light[2][3], double *sreflect, double *view, double *normal) {
 	color s;
+	double cons = 2 * dot_product(normal, light[LOCATION]);
+	double temp[3];
+	temp[0] = cons * normal[0] - light[LOCATION][0];
+	temp[1] = cons * normal[1] - light[LOCATION][1];
+	temp[2] = cons * normal[2] - light[LOCATION][2];
+	cons = pow(dot_product(temp, view), 16);
+	s.red = light[COLOR][RED] * sreflect[RED] * cons;
+	s.green = light[COLOR][GREEN] * sreflect[GREEN] * cons;
+	s.blue = light[COLOR][BLUE] * sreflect[BLUE] * cons;
+	//printf("Color: %d %d %d\n:", s.red, s.green, s.blue);
 	return s;
 }
-
 
 //limit each component of c to a max of 255
 void limit_color(color * c) {
 	if (c->red < 0) c->red = 0;
 	if (c->green < 0) c->green = 0;
 	if (c->blue < 0) c->blue = 0;
-
+	
 	if (c->red > 255) c->red = 255;
 	if (c->green > 255) c->green = 255;
 	if (c->blue > 255) c->blue = 255;
